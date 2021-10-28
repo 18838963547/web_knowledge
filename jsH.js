81dcf1e0fc6f301d364aa10034f31a58ccc60fde
zt <280717844@qq.com>
2021-10-28 21:14:59 +0800

    win

--- jsH.js
+++ jsH.js
@@ -1,13 +1,18 @@
-var a=10;
-var foo={
-  a:20,
-  bar:function(){
-      var a=30;
-      return this.a;
-    }
-}
-console.log(foo.bar()); // 20
-console.log((foo.bar)()); // 10
-console.log((foo.bar=foo.bar)()); //30
-console.log((foo.bar,foo.bar)());
+var promise1 = new Promise((resolve,reject)=>{
+    resolve()
+    reject()
+})
+var promise2 = new Promise((resolve,reject)=>{
+    resolve()
+    reject()
+})
+var promise3 = new Promise((resolve,reject)=>{
+    resolve()
+    reject()
+})
 
+var allPromis = Promise.all([promise1,promise2,promise3]).then((res)=>{
+    console.log(res)
+}).catch((error)=>{
+    console.log(error)
+})
