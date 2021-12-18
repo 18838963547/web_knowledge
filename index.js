fdd4148bb810922c0ee2593bb8889747bb87c3e2
zt <280717844@qq.com>
2021-12-18 17:47:43 +0800

    win

--- index.js
+++ index.js
@@ -16,28 +16,3 @@ function start (arr) {
     arrToTree(arr, null, tree)
     return tree
 }
-
-["push"].forEach(function (method) {
-    //   缓存数组原始的push方法
-    var original = Array.prototype[method];
-    var arrayMethods = Object.create(Array.prototype);
-    var newPush = function () {
-        var args = [],
-            len = arguments.length;
-        while (len--) args[len] = arguments[len];
-
-        var result = original.apply(this, args);
-        // data中每个数组都有一个__ob__的私有属性指向创建的Observer实例
-        var ob = this.__ob__;
-        // 监听数组的push调用，触发响应式系统
-        // 向所有依赖发送通知，告诉数组的值发生变化了
-        ob.dep.notify();
-        return result;
-    };
-    Object.defineProperty(arrayMethods, "push", {
-        value: newPush,
-        enumerable: !!enumerable,
-        writable: true,
-        configurable: true,
-    });
-});
