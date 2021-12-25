81650f24225c8b6255e929661556684a8dcc1bdb
zt <280717844@qq.com>
2021-12-25 23:32:56 +0800

    win

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-//  数组转树
-function arrToTree (arr, id = null, tree = []) {
-    // 遍历数组，
-    arr.forEach(element => {
-        if (element.pid == id) {
-            const newItem = { ...element, children: [] }
-            tree.push(newItem)
-            arrToTree(arr, element.pid, newItem.children)
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    let ans = [];
+    const len = nums.length;
+    if (nums == null || len < 3) return ans;
+    nums.sort((a, b) => a - b); // 排序
+    for (let i = 0; i < len; i++) {
+        if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+        if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
+        let L = i + 1;
+        let R = len - 1;
+        while (L < R) {
+            const sum = nums[i] + nums[L] + nums[R];
+            if (sum == 0) {
+                ans.push([nums[i], nums[L], nums[R]]);
+                while (L < R && nums[L] == nums[L + 1]) L++; // 去重
+                while (L < R && nums[R] == nums[R - 1]) R--; // 去重
+                L++;
+                R--;
+            }
+            else if (sum < 0) L++;
+            else if (sum > 0) R--;
         }
-    });
-    // 返回一个数组，这个数组的父节点id等于传进来的id
-}
-
-function start (arr) {
-    var tree = []
-    arrToTree(arr, null, tree)
-    return tree
-}
+    }
+    return ans;
+};
+console.log(threeSum([-4, -1, -1, 0, 1, 2]))
