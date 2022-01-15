263e058e2d5cb767f23e14d9e57fd77bd56a8d53
zt <280717844@qq.com>
2022-01-15 23:49:05 +0800

    添加webpack

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 /**
- * @param {number[]} nums
- * @return {number[][]}
+ * @param {number[][]} triangle
+ * @return {number}
  */
-var threeSum = function (nums) {
-    let ans = [];
-    const len = nums.length;
-    if (nums == null || len < 3) return ans;
-    nums.sort((a, b) => a - b); // 排序
-    for (let i = 0; i < len; i++) {
-        if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
-        if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
-        let L = i + 1;
-        let R = len - 1;
-        while (L < R) {
-            const sum = nums[i] + nums[L] + nums[R];
-            if (sum == 0) {
-                ans.push([nums[i], nums[L], nums[R]]);
-                while (L < R && nums[L] == nums[L + 1]) L++; // 去重
-                while (L < R && nums[R] == nums[R - 1]) R--; // 去重
-                L++;
-                R--;
-            }
-            else if (sum < 0) L++;
-            else if (sum > 0) R--;
+var minimumTotal = function (triangle) {
+    var len = triangle.length
+    var dp = []
+    dp.push(triangle[0])
+    for (var i = 1; i < len; i++) {
+        var ap = []
+        ap.push(dp[i - 1][0] + triangle[i][0])
+        for (var j = 1; j < i; j++) {
+            ap.push(Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j])
         }
+        ap.push(dp[i - 1][i - 1] + triangle[i][i])
+        dp.push(ap)
     }
-    return ans;
+    return Math.min(...dp[len - 1])
 };
-console.log(threeSum([-4, -1, -1, 0, 1, 2]))
+minimumTotal([[2], [3, 4], [6, 5, 9], [4, 4, 8, 0]])
