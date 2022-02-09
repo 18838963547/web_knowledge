4b523c8b919c5c2a548fc9a9e1bffdb1354be471
zt <280717844@qq.com>
2022-02-09 20:18:53 +0800

    win

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-/**
- * @param {number[][]} triangle
- * @return {number}
- */
-var minimumTotal = function (triangle) {
-    var len = triangle.length
-    var dp = []
-    dp.push(triangle[0])
-    for (var i = 1; i < len; i++) {
-        var ap = []
-        ap.push(dp[i - 1][0] + triangle[i][0])
-        for (var j = 1; j < i; j++) {
-            ap.push(Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j])
+function deleGate (element, eventType, selector, fn) {
+    element.addEventListener(eventType, e => {
+        let el = e.target
+        while (!el.matches(selector)) {
+            if (el === element) {
+                el = null
+                break
+            }
+            el = el.parentNode
         }
-        ap.push(dp[i - 1][i - 1] + triangle[i][i])
-        dp.push(ap)
-    }
-    return Math.min(...dp[len - 1])
-};
-minimumTotal([[2], [3, 4], [6, 5, 9], [4, 4, 8, 0]])
+        el && fn.call(el, e, el)
+    })
+    return element
+}
+let ul = document.querySelector('#ul');
+deleGate(ul, 'click', 'li', function (e) { console.log(e.target.innerText) })
