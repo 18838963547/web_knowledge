7fd4eef6c674cfd6e64a3e0de5b60bb5d8636f99
zt <280717844@qq.com>
2022-03-27 16:17:08 +0800

    window

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-// 继承
-
-function Parent () {
-    this.name = 'xiaoming'
-}
-Parent.prototype.eat = function () {
-    console.log(this.name)
-}
-
-function Sub () {
-
-}
-Sub.prototype = new Parent()
-Sub.prototype.milk = 'he'
-var sub1 = new Sub()
-// console.log(sub1.__proto__.__proto__ == Parent.prototype)
-// console.log(Sub.prototype.__proto__ == Parent.prototype) // sub1是Sub的实例对象。所以sub的隐式原型指向 Sub.prototype == new Parent(). 父类实例的隐式原型，指向的就是构造函数的原型
-// var sub2 = new Sub()
-// console.log(sub1)
-// console.log(sub2)
-
-const fxArr = ["One", "Two", "Three"]
-const fxArrs = fxArr.slice(0)
-fxArrs[1] = "love";
-console.log(fxArr) // ["One", "Two", "Three"]
-console.log(fxArrs) // ["One", "love", "Three"]
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {string[]}
+ */
+var wordBreak = function (s, wordDict) {
+    var len = s.length
+    var result = []
+    var map = new Map()
+    var wordSet = new Set(wordDict)
+    // 回溯
+    function backTrack (start, map) {
+        if (map.has(start)) {
+            return map.get(start)
+        }
+        var works = []
+        if (start == len) {
+            works.push([])
+        }
+        // 结束条件
+        for (var i = start + 1; i < len + 1; i++) {
+            var curStr = s.substring(start, i)
+            if (wordSet.has(curStr)) {
+                // 获取这个单词后面的所有可能情况
+                var nextWordBreaks = backTrack(i, map)
+                for (const item of nextWordBreaks) {
+                    work = [curStr, ...item]
+                    works.push(work)
+                }
+            }
+        }
+        map.set(start, works)
+        return works
+    }
+    const res = backTrack(0, map)
+    for (const item of res) {
+        result.push(item.join(" "))
+    }
+    return result
+};
+wordBreak("catsanddog", ["cat", "cats", "and", "sand", "dog"])
